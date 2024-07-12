3fa3cce8809537a9faaeef7af227fa6b401c4eb3
Brad McNew <mcnewbrad@gmail.com>
2024-07-12 17:49:55 -0400

    implement SearchForm style

--- src/features/searchPage/SearchForm.js
+++ src/features/searchPage/SearchForm.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import styles from './SearchForm.module.css';
 
 export function SearchForm ({ searchQuery, handleChange }) {
     return (
         <form>
-            <label htmlFor="search">Search for a subreddit</label>
+            <label htmlFor="search">Search for a subreddit: <span className={styles.navy}>r/</span></label>
             <input type="search" id="search" name="seach" value={searchQuery} onChange={handleChange}></input>
         </form>
     )
