d8c3eb9f67ea857781f4d0243553bf0ac8c6a686
Brad McNew <mcnewbrad@gmail.com>
2024-07-11 21:38:42 -0400

    implement crosspost title

--- src/features/searchResults/Post.js
+++ src/features/searchResults/Post.js
@@ -5,13 +5,17 @@ export default function Post({ post, image }) {
     const handleImageError = () => {
         setIsImageSupported(false);
     };
+    const hasCrosspost = post.crosspost_parent_list;
+    const crosspostTitle = hasCrosspost && post.crosspost_parent_list[0].title;
+    console.log("crosspost: " + crosspostTitle);
 
     return (
         <div key={post.id}>
             {(image && isImageSupported) && (
                 <div>
-                    <h3>{post.title}</h3>
+                    <h1>{post.title}</h1>
                     <p>{post.selftext}</p>
+                    <h2>{crosspostTitle}</h2>
                     <img src={image} alt={post.id + 'image'} onError={handleImageError} />
                 </div>
             )}
